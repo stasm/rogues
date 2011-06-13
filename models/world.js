1eb96e826f539d3e0fc8d7b69b344a473157d963
Stas Malolepszy <stas@mozilla.com>
2011-06-13 22:23:25 +0200

    Name the anonymous functions

--- models/world.js
+++ models/world.js
@@ -1,24 +1,24 @@
 // vim: ts=2 et sts=2 sw=2
 
-var World = exports.World = function() {
+var World = exports.World = function World() {
   this.players = [];
 }
 
 // @param {models.Player} player
-World.prototype.addPlayer = function(player) {
+World.prototype.addPlayer = function addPlayer(player) {
   this.players.push(player);
   this.announce(player.name + ' has joined!');
 }
 
 // @param {models.Player} player
-World.prototype.removePlayer = function(player) {
+World.prototype.removePlayer = function removePlayer(player) {
   this.players.splice(this.players.indexOf(player), 1);
   this.announce(player.name + ' has left!');
 }
 
 // @param {String} message
 // @param {models.Player} excluding
-World.prototype.broadcast = function(message, excluding) {
+World.prototype.broadcast = function broadcast(message, excluding) {
   for (var p, i=0; p = this.players[i]; i++) {
     if (p == excluding)
       continue
@@ -27,6 +27,6 @@ World.prototype.broadcast = function(message, excluding) {
 }
 
 // @param {String} message
-World.prototype.announce = function(message) {
+World.prototype.announce = function announce(message) {
   this.broadcast('> ' + message)
 }
